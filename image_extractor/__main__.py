add65f844425fe50738947f2d2a70b3a08848c98
Stefan Bergmann <bergmannstefan@live.de>
2018-05-19 12:25:29 +0200

    added argument to use compare tool

--- image_extractor/__main__.py
+++ image_extractor/__main__.py
@@ -1,17 +1,30 @@
 import sys
 import argparse
+import compare
+import cv2
+import numpy as np
 
 
 def main(args=None):
-    if args.path:
-        print("given path: %s" % args.path)
-    print("This is the main routine.")
+
+    img = cv2.imread(args.image, cv2.IMREAD_COLOR)
+
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    if args.compare:
+        compareable = cv2.imread(args.compare, cv2.IMREAD_COLOR)
+    else:
+        print("This is the main routine.")
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("image", help="path to the image of the photo album page")
     parser.add_argument("-p", "--path", help="path to the directory to store the results", type=str)
+    parser.add_argument("-c", "--compare", help="path to the image to compare the given image to", type=str)
     args = parser.parse_args()
 
     main(args)
